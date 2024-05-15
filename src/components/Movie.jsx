b5e289e6a752885aed15f15024cf70057326708b
Burak <ozb5076@gmail.com>
2024-05-15 03:18:12 +0300

    düzenleme yapıldı

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,15 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import '../style/movie.css'
+import '../style/movie.css';
 
 function Movie() {
     const [movieList, setMovieList] = useState([]);
 
     const fetchMovies = async () => {
         const res = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=4809c298151f2a5613663465a0b0fa88');
-            const json = await res.json();
-            console.log(json.results);
-            setMovieList(json.results);
-        
+        const json = await res.json();
+        setMovieList(json.results);
     };
 
     useEffect(() => {
@@ -18,23 +16,25 @@ function Movie() {
 
     return (
         <div>
-            <h1>Movie List</h1>
-            <div className="movie-container">
+            <h1 className='baslik'>Movie List</h1>
+            <div className="cards">
                 {movieList.length > 0 ? (
                     movieList.map(movie => (
                         <div key={movie.id} className="movie-card">
-                            <h2>{movie.title}</h2>
-                            <p>Yayınlanma Tarihi:{movie.release_date}</p>
-                            {movie.poster_path ? (
-                                <img style={{height:"350px",width:"350px"}} src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
-
-                            ) : (
-                                <p>Fotoğtafi yoktur.</p>
-                            )}
+                            <img style={{ height: "500px", width: "400px" }} src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
+                            <div className="card-text">
+                                <div className='hidden'>
+                                    <span>Detaylar</span> <span>İzle</span>
+                                </div>
+                                <h2>{movie.title}</h2>
+                                <div className="movie-details">
+                                    <span className="detail">IMDB: {movie.vote_average}</span>
+                                </div>
+                            </div>
                         </div>
                     ))
                 ) : (
-                    <p>Yükleniyor</p>
+                    <p>Loading...</p>
                 )}
             </div>
         </div>
