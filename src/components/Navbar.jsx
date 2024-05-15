e8e364812cc0a3e943ae16077777f9824d4580ae
tufan <tufankorkmazel3@gmail.com>
2024-05-15 03:22:57 +0300

    navbar yapıldı.

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import '../style/navbar.css'
+import moviesLogo from '../assets/appleTvPlusLogo.png'
+
+function Navbar() {
+    return (
+        <>
+            <div className='containerBar'>
+                <div className='logo'>
+                    <img src={moviesLogo} alt="" />
+                </div>
+                <div>
+                    <ul className='navbar'>
+                        <li> <a href="#">Anasayfa</a> </li>
+                        <li> <a href="#">Filmler</a> </li>
+                        <li> <a href="#">Diziler</a> </li>
+                    </ul>
+                </div>
+
+            </div>
+
+
+        </>
+    )
+}
+
+export default Navbar
